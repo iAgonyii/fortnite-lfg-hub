8f5966391ef9d9326d239cd130ee3413cdeaa39f
Teneij,Roy H.B <roy.teneij@student.fontys.nl>
2019-10-28 13:46:30 +0100

    Initial commit

---
+++ Fortnite-LFG-Hub/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fortnite_LFG_Hub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
