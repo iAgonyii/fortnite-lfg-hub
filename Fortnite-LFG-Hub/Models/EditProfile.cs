d5108b47a445318595518ad0036adc95bf48f539
Teneij,Roy H.B <roy.teneij@student.fontys.nl>
2019-12-18 10:21:48 +0100

    csproj files

---
+++ Fortnite-LFG-Hub/Models/EditProfile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fortnite_LFG_Hub.Models
+{
+    public class EditProfile: IProfile
+    {
+    }
+}
