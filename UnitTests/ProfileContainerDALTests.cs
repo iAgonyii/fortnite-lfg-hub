56773e371ec63669933b52a22bf1e155dcf0d814
Roy Teneij <411850@student.fontys.nl>
2019-12-27 03:50:14 +0100

    Added unit tests for all current logic & data functions

--- UnitTests/ProfileContainerDALTests.cs
+++ UnitTests/ProfileContainerDALTests.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using BusinessLayer;
+using BusinessLayerContainer;
+using DataLayerDTO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace UnitTestData
 {
-    class ProfileContainerDALTests
+    [TestClass]
+    public class ProfileContainerDALTests
     {
+        private ProfilesContainer profilesContainer = new ProfilesContainer();
+
+        [TestMethod]
+        public void GetProfileDataForId()
+        {
+            int id = 66;
+            Profile result = profilesContainer.GetProfileData(id);
+            Assert.AreEqual("UnitTest1", result.Username);
+        }
+
+        [TestMethod]
+        public void GetProfileDataForUnknownIdThrowsException()
+        {
+            bool caught;
+            int id = 999999;
+            try
+            {
+                Profile result = profilesContainer.GetProfileData(id);
+                caught = false;
+            }
+            catch (Exception)
+            {
+
+                caught = true;
+            }
+            Assert.IsTrue(caught);
+        }
     }
 }
