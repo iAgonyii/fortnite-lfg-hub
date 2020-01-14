14fae702966e5e2bd699850999cd7eb90a36b345
Teneij,Roy H.B <roy.teneij@student.fontys.nl>
2020-01-14 12:00:27 +0100

    Extracted method: making profile from dto, this also fixed duplicate code

--- BusinessLayerContainers/ProfilesContainer.cs
+++ BusinessLayerContainers/ProfilesContainer.cs
@@ -14,15 +14,11 @@ namespace BusinessLayerContainer
         public List<Profile> GetProfiles()
         {
             profiles = new List<Profile>();
-            CommentsContainer cContainer = new CommentsContainer();
-            AchievementsContainer aContainer = new AchievementsContainer();
 
             List<ProfileDTO> profileDtos = commands.GetProfiles();
-            profileDtos.ForEach(pdto =>
+            profileDtos.ForEach(dto =>
             {
-                Profile profile = new Profile(pdto);
-                profile.Comments = cContainer.GetComments(profile.UserId);
-                profile.Achievements = aContainer.GetAchievementsForProfile(profile.UserId);
+                Profile profile = MakeFullProfileFromDto(dto);
                 profiles.Add(profile);
             });
 
@@ -31,15 +27,22 @@ namespace BusinessLayerContainer
 
         // Get all the data of a profile. This includes: achievements, socials and comments.
         public Profile GetProfileData(int id)
+        { 
+            ProfileDTO dto = commands.GetProfileData(id);
+            Profile profile = MakeFullProfileFromDto(dto);
+
+            return profile;
+        }
+
+        private Profile MakeFullProfileFromDto(ProfileDTO dto)
         {
             CommentsContainer cContainer = new CommentsContainer();
             AchievementsContainer aContainer = new AchievementsContainer();
 
-            ProfileDTO dto = commands.GetProfileData(id);
             Profile profile = new Profile(dto);
-
             profile.Comments = cContainer.GetComments(profile.UserId);
             profile.Achievements = aContainer.GetAchievementsForProfile(profile.UserId);
+
             return profile;
         }
     }
